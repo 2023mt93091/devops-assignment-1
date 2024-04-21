13c9d6b690194f5575c00d9b9ced7f5e3676eee8
Sumeet Godse <sumeetgodse1997@gmail.com>
2024-04-21 11:54:24 +0530

    some raw changes

--- devops/src/App.jsx
+++ devops/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        Add some raw code to the newly created branch : branch-1
         </p>
       </div>
       <p className="read-the-docs">
